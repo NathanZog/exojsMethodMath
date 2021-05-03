0af4c020b599bc327bf0a3fd81366b336709cd6e
NathanZog <81653297+NathanZog@users.noreply.github.com>
2021-05-03 10:14:41 +0200

    save exo5 js

---
+++ src/js/main.js
@@ -0,0 +1,27 @@
+// 1 à 5
+let nombre = 8.56
+console.log(Math.round(nombre));
+console.log(Math.ceil(nombre));
+console.log(Math.floor(nombre));
+console.log(Math.trunc(nombre));
+//6
+console.log(Math.random());
+//7
+let random = Math.random()*100
+console.log(random);
+//8 
+let random2 = Math.round(Math.random()*100);
+console.log(random2)
+//9
+let nombre2 = 8
+console.log(Math.pow(nombre2,2))
+//10
+let nombre3 = 9
+console.log(Math.sqrt(nombre3))
+//11
+let nombre4 = -1
+console.log(Math.abs(nombre4))
+//12
+console.log(Math.max(-2,1000,8,57))
+//13
+console.log(Math.min(-2,1000,8,57))
